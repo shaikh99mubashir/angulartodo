75095bf8b77db0f9903d1382087a8fe7ddad88f0
shaikh99mubashir <shaikh99mubashir@gmail.com>
2023-06-21 16:43:36 +0500

    update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,37 +1,59 @@
 import { Component } from '@angular/core';
 
+interface TodoItem {
+  id:number;
+  value: string;
+}
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
+
 export class TodoComponent {
-  public displayTodo: string[] =[];
-  
-  onSave(value: string , inputField : HTMLInputElement) {
+  public displayTodo: TodoItem[] = [];
+  buttonValue = 'Add';
+
+  id=0;
+  onSave(value: any , inputField : HTMLInputElement):any {
+    console.log('displayTodo',this.displayTodo);
+    console.log('value===>',value);
     if(value == ''){
       alert('Add Todo')
     }
+    else if(this.buttonValue == 'update'){
+      this.buttonValue = 'Add'
+      this.displayTodo[this.id].value=value;    
+
+    }
     else{
-      this.displayTodo.push(value);
+      const todoItem: TodoItem = { id:this.displayTodo.length,value:value };
+      this.displayTodo.push(todoItem);
       inputField.value= ''
     }
-    // console.log('value',this.displayTodo);
+  }
+
+
+  
+  edit(index:number,value:any,inputField : HTMLInputElement,){
+    this.buttonValue = 'update'
+    console.log('editIndex',index);
+    console.log('editvalue',value);
+    console.log('displayTodo',this.displayTodo);
+    console.log('inputField',inputField.value);
+    inputField.value= value.value;
+    this.id=index;
+    console.log('this.displayTodo[index].value',this.displayTodo[index].value);
+    
+
+
+
   }
 
   delete(index:number){
     console.log('i',index);
     this.displayTodo = this.displayTodo.filter((e,i)=> i !== index)
-  }
-  
-  edit(index:number){
-    console.log('editIndex',index);
-    let updatedValue = prompt('Edit todo')
-    console.log('updatedValue',updatedValue);
-
-    if (updatedValue !== null) {
-      this.displayTodo[index] = updatedValue;
-    }
-    
+   
   }
 }
